360d955b8b46ec8e95eeab6828453dab15215c45
Jonas Wiklund <jwiklund@spotify.com>
2015-04-02 14:16:15 +0200

    Allow running without server.conf

--- src/main/java/com/spotify/server/Main.java
+++ src/main/java/com/spotify/server/Main.java
@@ -11,7 +11,9 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 public class Main {
@@ -27,22 +29,35 @@ public class Main {
     resources.setResourceBase(".");
     handlers.addHandler(resources);
 
-    ServletContextHandler servlets = new ServletContextHandler();
-    Yaml yaml = new Yaml();
-    Map<?, ?> config = (Map<?, ?>) yaml.load(new FileInputStream("server.conf"));
-    for (Map.Entry<?, ?> proxy : config.entrySet()) {
-      if (((String) proxy.getKey()).startsWith("proxy_")) {
-        String name = ((String) proxy.getKey()).substring("proxy_".length());
-        ServletHolder proxyHandler = new ServletHolder(name, new ProxyServlet());
-        proxyHandler.setInitParameter("url", (String) proxy.getValue());
-        servlets.addServlet(proxyHandler, "/" + name);  
-      }
-    }
-    handlers.addHandler(servlets);
+    handlers.addHandler(configureProxy());
 
     Server server = new Server(8080);
     server.setHandler(handlers);
     server.start();
     server.join();
+  }
+
+  private static ServletContextHandler configureProxy() throws FileNotFoundException {
+    ServletContextHandler servlets = new ServletContextHandler();
+    if (!new File("server.conf").exists()) {
+      return servlets;
+    }
+    Yaml yaml = new Yaml();
+    Object loaded = yaml.load(new FileInputStream("server.conf"));
+    if (!(loaded instanceof Map)) {
+      throw new Error("Invalid YAML, got " + loaded.getClass() + ", expected map (see README.md for valid yaml example)");
+    }
+    Map<?, ?> config = (Map<?, ?>) loaded;
+    if (config.entrySet() != null) {
+      for (Map.Entry<?, ?> proxy : config.entrySet()) {
+        if (((String) proxy.getKey()).startsWith("proxy_")) {
+          String name = ((String) proxy.getKey()).substring("proxy_".length());
+          ServletHolder proxyHandler = new ServletHolder(name, new ProxyServlet());
+          proxyHandler.setInitParameter("url", (String) proxy.getValue());
+          servlets.addServlet(proxyHandler, "/" + name);
+        }
+      }
+    }
+    return servlets;
   }
 }
